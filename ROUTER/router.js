5de0312a2729d92e1671a24ffe4bea12ae9c7c94
rodrigoatg <rodrigoatg@hotmail.com>
2022-09-01 22:26:55 -0300

    faltantes

--- ROUTER/router.js
+++ ROUTER/router.js
@@ -4,24 +4,45 @@ const { Router } = express;
 export const routerProductos = new Router();
 export const routerCarrito = new Router();
 
+
 routerProductos.use(express.json());
 routerCarrito.use(express.json());
 
 //PRODUCTOS
-const personas = [];
+const productos = [];
 
 routerProductos.get('/:id', (req, res) =>{
-    res.json(personas);
+    const { id } = req.params;
+    const productoBuscado = {};
+    if (id === "" || isNull(id)){
+        res.send({productos: productos.getAll()});
+    }else{
+        productoBuscado = productos.getById(id) ;
+        if(!isNull(productoBuscado)){
+            res.send({ buscado: productoBuscado });
+        }else{
+            res.send('<h3 style="color:red;">Producto no encontrado</h3>')
+        }
+    }
 });
-routerPersonas.post('/', (req, res) => {
-    personas.push(req.body)
-    res.json(req.body)
+routerProductos.post('/', (req, res) => {
+    const { producto } = req.body;
+    const newId = parseInt(productos.length) + 1;
+    productos.push({id: newId, ...producto});
+    res.send(productos[parseInt(productos.length) - 1]);
+    //guardar al archivo
 });
 routerProductos.put('/:id', (req, res) =>{
-    res.json(personas);
+    const { producto } = req.body;
+    const { id } = req.params;
+    const posProductoBuscado = 0;
+    
+    posProductoBuscado = productos.getPosById(id) ;
+    productos[pos] = producto;
 });
 routerProductos.delete('/:id', (req, res) =>{
-    res.json(personas);
+    const { id } = req.params;
+    productos.deleteByID(id);
 });
 
 //CARRITO
---
+++ api/carrito.js
@@ -0,0 +1,64 @@
+export class CarritoApi {
+    constructor() {
+        this.carrito = []
+        this.id = 0
+    }
+
+    save(objeto){
+
+        const nuevoId = this.info[this.info.length() - 1] + 1;
+
+        const nuevoObjeto = {nuevoId, ...objeto};
+
+        this.info = {...this.info, ...nuevoObjeto};
+        this.fs.writeFile(this.name, JSON.stringify(this.info, null, 2), error => {
+            if (error) {
+                throw new Error('Error en escritura');
+            }
+            console.log('Escritura exitosa');
+            return nuevoId;
+        })        
+    }
+
+    getById(idRecibido){
+        for( const producto of this.info){
+            if(producto.id === idRecibido){
+                return producto;
+            }
+        }
+        return null
+    }
+
+    getAll(){
+        return this.info
+    }
+
+    deleteByID(idRecibido){
+        let i = 0;
+        for( const producto of this.info){
+            if(producto.id === idRecibido){
+                this.info.slice(i,1);//esto funciona en array
+            }
+            i++;
+        }
+        for(let i = 0; i < this.info.length(); i++){
+            if(this.info.id === idRecibido){
+                this.info.splice(i,1);
+            }
+        }
+        this.fs.writeFile(this.name, JSON.stringify(this.info, null, 2), error => {
+            if (error) {
+                throw new Error('Error en eliminación del objeto.');
+            }
+            console.log('Escritura exitosa');
+        })        
+    }
+
+    deleteAll(){
+        this.fs.writeFile(this.name, "", error => {
+            if (error) {
+                throw new Error('Error borrando el archivo.');
+            }
+        });
+    }
+}
